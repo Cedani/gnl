1f6bbc077124676a2f4de48a4e2b220080076f96
cedrick <cedanikak@gmail.com>
2019-12-16 16:35:37 +0100

    adding(master): initial commit

---
+++ get_next_line.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** get_next_line
+** File description:
+** get_next_line
+*/
+
+#include <stddef.h>
+
+char *get_next_line(int fd) {
+    if (fd == -1)
+        return (NULL);
+}
