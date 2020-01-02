855a05e3d3e5a8f96e2569f8ff9e6112c52a20c9
cedrick <cedanikak@gmail.com>
2020-01-02 22:18:01 +0100

    fix(master): fix bad returning of buffer_to_give function in case of only a '\n' in a line

--- get_next_line.c
+++ get_next_line.c
@@ -37,7 +37,8 @@ char *buffer_to_give(int *ind, char *cont)
         return NULL;
     for (; cont[*ind] && ok != 2; *ind += 1) {
         if (cont[*ind] == '\n' && ok != 1) {
-            return ("\n");
+            buffer[0] = '\n';
+            ok == 2;
         }
         else if (cont[*ind] == '\n' && ok == 1)
             ok = 2;
@@ -71,4 +72,16 @@ char *get_next_line(int fd)
     }
     tmp = buffer_to_give(&index, container);
     return (tmp);
+}
+
+int main(void) {
+    int fd = open("test", O_RDONLY);
+    char *buffer = get_next_line(fd);
+
+    while (buffer) {
+        printf("%s\n", buffer);
+        free(buffer);
+        buffer = get_next_line(fd);
+    }
+    return (0);
 }
