9a6ca1675cc55a945868ba3b6009c26d2c3991b0
cedrick <cedanikak@gmail.com>
2020-01-02 22:38:24 +0100

    adding(master): adding criterions test

--- get_next_line.h
+++ get_next_line.h
@@ -22,5 +22,4 @@
 char *my_strcat(char *str1, char *str2, int n);
 char *buffer_to_give(int *index, char *container);
 char *get_next_line(int fd);
-void reset_static(char **container, int *index, int fd2, int *init);
 #endif /* !GET_NEXT_LINE */
